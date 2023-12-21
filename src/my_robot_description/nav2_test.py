987d16de312004a7cee0dfc5c832b66d4235154e
ant1234 <antonyvidetta1986@gmail.com>
2023-12-21 17:07:59 +1300

    Add initial position and goal position programmatically

--- src/my_robot_description/nav2_test.py
+++ src/my_robot_description/nav2_test.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+#  notes :
+# quaternions use radians for the angle angle ie.
+# pi is 3.14 which is equal to 180 degrees 
+# pi/2 is 1.57 which is equal to 90 degrees
+# 90 degrees anti clockwise is 1.57
+# 90 degrees clockwise is -1.57
+
 import rclpy
 from nav2_simple_commander.robot_navigator import BasicNavigator
 from geometry_msgs.msg import PoseStamped
@@ -23,11 +30,28 @@ def main():
     initial_pose.pose.orientation.y = q_y
     initial_pose.pose.orientation.z = q_z
     initial_pose.pose.orientation.w = q_w
-
     nav.setInitialPose(initial_pose)
 
     # wait for nav2 to be active
     nav.waitUntilNav2Active()
+
+    # send navigation goal
+    q_x, q_y, q_z, q_w = tf_transformations.quaternion_from_euler(0.0, 0.0, 1.57)
+    goal_pose = PoseStamped()
+    goal_pose.header.frame_id = "map"
+    goal_pose.header.stamp = nav.get_clock().now().to_msg()
+    goal_pose.pose.position.x = 3.5
+    goal_pose.pose.position.y = 1.0
+    goal_pose.pose.position.z = 0.0
+    goal_pose.pose.orientation.x = q_x
+    goal_pose.pose.orientation.y = q_y
+    goal_pose.pose.orientation.z = q_z
+    goal_pose.pose.orientation.w = q_w
+    nav.goToPose(goal_pose)
+
+    while not nav.isTaskComplete():
+        feedback = nav.getFeedback()
+        print(feedback)
 
     rclpy.shutdown()
 
